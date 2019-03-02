aa1e58ad8db8238935f7ef604d811b6184af78ab
Eli <h5rdly@protonmail.ch>
2019-03-02 16:14:44 +0200

    piping properly on linux, advancing closing bracket

--- pepe.py
+++ pepe.py
@@ -1,6 +1,7 @@
-import tempfile #, cmd
+import tempfile, readline #, cmd
 from subprocess import Popen, PIPE
 from configparser import ConfigParser  #, ExtendedInterpolation, BasicInterpolation
+from collections import Counter
 
 
 def get_config(conf_file = 'pepe.ini'):
@@ -15,7 +16,7 @@ def get_config(conf_file = 'pepe.ini'):
     
     config = ConfigParser()
     try:
-        config.read(conf_file)
+        config.read_file(open('conf_file'))
     except Exception as e:
         print ('Exception reading config file: ', e)
         config.read_string(default_config)
@@ -29,7 +30,7 @@ def get_config(conf_file = 'pepe.ini'):
 def interpret(code, compiler = 'clang'):
     ''' '''
 
-    interpret = Popen(('clang'), stdin = PIPE, stdout = PIPE, stderr = PIPE)
+    interpret = Popen((compiler, '-xc++', '-'), stdin = PIPE, stdout = PIPE, stderr = PIPE)
     res, err = interpret.communicate(code.encode('utf-8'))
 
 
@@ -42,25 +43,29 @@ def mainloop(prompt = 'pepe> ', config = None):
     # Setup config and file for managing the incoming code
     config = config or get_config()
     pepe_file = tempfile.SpooledTemporaryFile(mode='a+t')
-    pepe_file.write('\n'.join(('#include <stdio.h>', 'int main()', '{', '')))
-    
+    pepe_file.write('\n'.join(('#include <stdio.h>', 'int main()', '{', '}')))
     compiler = config['Paths']['compiler']
     tab = ' ' * 4
-    tabdepth = 1   # Counts how many tabs to add to keep the pepe code formatted for viewing
-    
+    block_depth = 1  
 
     while True:
-        line = input(prompt)
-        if not line.endswith('{'):
-            # Don't have to add ;
-            pepe_file.write(tab * tabdepth + line + ';\n}')
-    
-            # Interpret pepe
+        line = input(prompt + tab*(block_depth - 1))
+        # Remove finishing '}' and add new line
+        pepe_file.seek(pepe_file.tell() - 1)  # 1 back from current position
+        pepe_file.truncate()
+        pepe_file.write(tab * block_depth + line + ';\n')   # No need to add ;
+
+        counter = Counter(line)
+        block_depth += line.count('{') - line.count('}')
+        if not block_depth > 1:
+            # Close up and interpret pepe
+            pepe_file.write('}')
             pepe_file.seek(0)
             code = pepe_file.read()
             print ("code:\n", code)
             res, err = interpret(code, compiler)
             print ("result:\n", res)
+            print ("error:\n", err)
 
 
 if __name__=='__main__':
